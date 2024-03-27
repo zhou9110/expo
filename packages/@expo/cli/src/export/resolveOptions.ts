430203107ac5105a9f0523c3e9bde5add00fe171
Kim Brandwijk <kim.brandwijk@gmail.com>
2024-03-27 16:45:05 -0500

    linting

--- packages/@expo/cli/src/export/resolveOptions.ts
+++ packages/@expo/cli/src/export/resolveOptions.ts
@@ -46,7 +46,7 @@ export function resolvePlatformOption(
       )}].`;
 
       if (platform === 'web') {
-        error += ' ' + learnMore("https://docs.expo.dev/guides/customizing-metro/#web-support");
+        error += ' ' + learnMore('https://docs.expo.dev/guides/customizing-metro/#web-support');
       }
 
       throw new CommandError('BAD_ARGS', error);
